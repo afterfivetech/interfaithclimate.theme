ead73ee55a884233d6f531a4d23104c45d83ad2b
joemariedimzon <joemariedimzon@gmail.com>
2015-02-06 16:37:12 +0800

    theme files adjustment

--- interfaithclimate/theme/static/js/app.js
+++ interfaithclimate/theme/static/js/app.js
@@ -2,3 +2,8 @@ $(document).foundation();
 $(".select-menu-button").click(function(){
     $("ul.theme_button").slideToggle();
 });
+$('#mainmenu_area > ul#mainmenu li').each(function(){
+    if ($(this).attr('class')=='selected'){
+        $("a.select-menu-button > span.holder").html($(' > a > span').html());	
+    }
+});
